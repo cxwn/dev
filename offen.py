41da16a2c30569b51750385f3a6571345773b9cc
mrivandu <mrivandu@hotmail.com>
2019-04-21 23:55:26 +0800

    Update

---
+++ offen.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+import urllib.request
+DATA=b'some data'
+req = urllib.request.Request(url='http://125.88.146.204', data=DATA,method='PUT')
+f = urllib.request.urlopen(req)
+print(f.status)
+print(f.reason)
+
+125.88.146.204
+114.248.118.240
+
+remote addr: 125.88.146.76:15350
+
+http://admin.sqqmall.com/download/qssqian.apk
+itms-services://?action=download-manifest&url=https://d.dtvai.com/vtsuser/qssqian/vts.plist
+
+
+IP：119.249.54.252
+远程端口：22
+账号：root
+密码：sxlgXzoJaJfS. 
