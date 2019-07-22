c89311f6bd825ca9ddc1bfd4fb0740f2f1aae166
mrivandu <mrivandu@hotmail.com>
2019-07-22 14:08:55 +0800

    pdate

---
+++ install_docker-compose.sh
@@ -0,0 +1,4 @@
+# 
+
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
+cp docker-compose /usr/local
--- offen.py
+++ offen.py
@@ -1,13 +1,7 @@
 # -*- coding:utf-8 -*-
-import urllib.request
-DATA=b'some data'
-req = urllib.request.Request(url='http://125.88.146.204', data=DATA,method='PUT')
-f = urllib.request.urlopen(req)
-print(f.status)
-print(f.reason)
-
-ip=('125.88.146.204','114.248.118.240')
-
-remote_addr='125.88.146.76:15350'
-
-url=['http://admin.sqqmall.com/download/qssqian.apk']
+import tkinter
+windows = tkinter.Tk()
+windows.title("My name is ivan.")
+lbl = tkinter.Label(windows, text = "Text")
+lbl.grid(column = 0, row = 0)
+windows.mainloop()
