f06a1fe813561f4b0ff8e689a3bf70efd3050906
mrivandu <mrivandu@hotmail.com>
2020-08-03 14:41:28 +0800

    Update

--- apps/mysite/mysite/views.py
+++
@@ -1,6 +0,0 @@
-# -*- coding:utf-8 -*-
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def login(request):
-    return HttpResponse("login successful!")
--- apps/mysite/test.py
+++
@@ -1,5 +0,0 @@
-# -*- ccoding:utf-8 -*-
-name = 'ivan'
-a = f'my {name}'
-b = 'du'
-print('%s' % a)
