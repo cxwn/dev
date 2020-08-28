ae19c0dc2c76f581f50494d4dd92a2705e90ba00
mrivandu <mrivandu@hotmail.com>
2020-08-28 18:47:05 +0800

    Update

---
+++ udfs/udfs.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello():
+    return '<h1>My name is ivan du.<h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
